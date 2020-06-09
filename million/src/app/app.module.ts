07c5abb5e78996ea775f627a0d4fd8b0a34f78e0
Everton Avelino <everton-vitor@hotmail.com>
2020-06-08 23:29:23 -0300

    iniciando front end angular 9

--- million/src/app/app.module.ts
+++ million/src/app/app.module.ts
@@ -3,16 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './views/home/home.component';
-import { LoginComponent } from './views/login/login.component';
-import { ClientesComponent } from './views/clientes/clientes.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    LoginComponent,
-    ClientesComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
--- million/src/environments/environment.prod.ts
+++ million/src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
 export const environment = {
-  apiHost: "colocar url da api no heroku",
   production: true
 };
